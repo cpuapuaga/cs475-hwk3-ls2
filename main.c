bf4e437101cd92f3e7b0c89cf1f4be0c31a91c2f
cpuapuaga <123214892+cpuapuaga@users.noreply.github.com>
2023-02-10 00:16:05 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "stack.h"
 #include "ls2.h"
 
 /**
  * Main function
  * Usage: ls2 <path> [exact-match-pattern]
+ * 
+ * Unfinished code. Does not include mode 2
  */
 int main(int argc, char* argv[]) {
-	// stack stores the lines to print out
-	stack_t *s = initstack();
-
-	push(s, "Hello1");
-	push(s, "Hello2");
-	push(s, "Hello3");
-
-	// print stack
-	printstack(s);
-
-	// free up stack
-	freestack(s);
+	char path[strlen(argv[1])];
+	//mode 1
+	if(argc == 2){
+		strcpy(path, argv[1]);
+		validDir(path);
+	}
 	return 0;
 }
